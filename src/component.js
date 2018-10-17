602f2511414641612b82f39ea66e5029dc4e8aaa
VitaliZinkevich <vitalizinkevich@gmail.com>
2018-10-18 01:39:45 +0300

    some tests with forms

--- src/component.js
+++ src/component.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import {Row, Input,Dropdown, Button, Navbar, NavItem, Icon, Footer } from 'react-materialize';
+
 
 const Test = ()=>{
     return (
-        <div>TEST</div>
+        <main>
+
+            <Row>
+                <Input s={6} 
+                onChange={function(e, value, ...rest) {
+                    console.log (e, value)
+                }}
+                label="First Name" validate><Icon>account_circle</Icon></Input>
+                <Input s={6} label="Telephone" validate type='tel'><Icon>phone</Icon></Input>
+            </Row>
+
+            <Row>
+                <Input name='on' type='date' onChange={function(e, value) {
+                    console.log (e, value)
+                }} />
+            </Row>
+        </main>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Dropdown, Button, Navbar, NavItem, Icon } from 'react-materialize';
+import {Row, Input,Dropdown, Button, Navbar, NavItem, Icon, Footer } from 'react-materialize';
 import Test from './component'
+
+
 const App = ({name}) =>{
   return (
-    <div className='container'>
+    <div className='container appbody'>
         <Navbar brand='logo' right>
           <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
           <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
           <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
           <NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
         </Navbar>
-        <Test></Test>
+
+        <Test>
+        
+
+        </Test>
+
+        <Footer copyrights="&copy 2015 Copyright Text"
+            moreLinks={
+              <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+            }
+            links={
+              <ul>
+                <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+              </ul>
+            }
+            
+          >
+              <h5 className="white-text">Footer Content</h5>
+              <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+        </Footer>
     </div>
   
   
